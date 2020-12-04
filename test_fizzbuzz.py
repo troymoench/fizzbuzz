141422937fc5663d02c97f8e82e9981d7d1a5b80
Troy Moench <brewersarenumber1@gmail.com>
2020-12-03 20:44:51 -0600

    Added some more asserts to tests. Tests are failing

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -2,12 +2,19 @@ from fizzbuzz import fizzbuzz
 
 def test_number_divisible_by_3_returns_fizz():
     assert fizzbuzz(3) == "fizz"
+    assert fizzbuzz(6) == "fizz"
+    assert fizzbuzz(15) == "fizz"
 
 def test_number_divisible_by_5_returns_buzz():
     assert fizzbuzz(5) == "buzz"
+    assert fizzbuzz(10) == "buzz"
+    assert fizzbuzz(15) == "buzz"
 
 def test_number_divisible_by_3_and_5_returns_fizzbuzz():
     assert fizzbuzz(15) == "fizzbuzz"
+    assert fizzbuzz(30) == "fizzbuzz"
 
 def test_number_not_divisible_by_3_or_5_returns_number():
     assert fizzbuzz(1) == "1"
+    assert fizzbuzz(2) == "2"
+    assert fizzbuzz(4) == "4"
