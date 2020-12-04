e43fa35cde35c508cb626b625ec7623d92d74078
Troy Moench <brewersarenumber1@gmail.com>
2020-12-03 20:23:02 -0600

    Fixed bug

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,10 @@
 
 def fizzbuzz(i: int) -> str:
-    if i % 3 == 0:
+    if i % 3 == 0 and i % 5 == 0:
+        return "fizzbuzz"
+    elif i % 3 == 0:
         return "fizz"
     elif i % 5 == 0:
         return "buzz"
-    elif i % 3 == 0 and i % 5 == 0:
-        return "fizzbuzz"
     else:
         return str(i)
