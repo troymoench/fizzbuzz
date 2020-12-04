784bd993f3f454a15ff846469e63f795124f75a5
Troy Moench <brewersarenumber1@gmail.com>
2020-12-03 20:14:19 -0600

    An initial buggy implementation.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,10 @@
 
 def fizzbuzz(i: int) -> str:
-    pass
+    if i % 3 == 0:
+        return "fizz"
+    elif i % 5 == 0:
+        return "buzz"
+    elif i % 3 == 0 and i % 5 == 0:
+        return "fizzbuzz"
+    else:
+        return str(i)
