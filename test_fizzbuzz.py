f952a9cc6b7f5926e2139fc4e9a8260c2ca79ae9
Troy Moench <brewersarenumber1@gmail.com>
2022-09-11 15:57:35 -0500

    Start using Hypothesis property testing

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,24 +1,40 @@
+from hypothesis import given
+from hypothesis import strategies as st
+
 from fizzbuzz import fizzbuzz
 
 
-def test_number_divisible_by_3_but_not_5_returns_fizz():
-    assert fizzbuzz(3) == "fizz"
-    assert fizzbuzz(6) == "fizz"
-    assert fizzbuzz(18) == "fizz"
+def numbers_divisible_by_3_but_not_5():
+    return st.just(3) | st.just(6) | st.just(18)
 
 
-def test_number_divisible_by_5_but_not_3_returns_buzz():
-    assert fizzbuzz(5) == "buzz"
-    assert fizzbuzz(10) == "buzz"
-    assert fizzbuzz(20) == "buzz"
+def numbers_divisible_by_5_but_not_3():
+    return st.just(5) | st.just(10) | st.just(20)
 
 
-def test_number_divisible_by_3_and_5_returns_fizzbuzz():
-    assert fizzbuzz(15) == "fizzbuzz"
-    assert fizzbuzz(30) == "fizzbuzz"
+def numbers_divisible_by_3_and_5():
+    return st.just(15) | st.just(30)
 
 
-def test_number_not_divisible_by_3_or_5_returns_number():
-    assert fizzbuzz(1) == "1"
-    assert fizzbuzz(2) == "2"
-    assert fizzbuzz(4) == "4"
+def numbers_not_divisible_by_3_or_5():
+    return st.just(1) | st.just(2) | st.just(4)
+
+
+@given(num=numbers_divisible_by_3_but_not_5())
+def test_number_divisible_by_3_but_not_5_returns_fizz(num: int):
+    assert fizzbuzz(num) == "fizz"
+
+
+@given(num=numbers_divisible_by_5_but_not_3())
+def test_number_divisible_by_5_but_not_3_returns_buzz(num: int):
+    assert fizzbuzz(num) == "buzz"
+
+
+@given(num=numbers_divisible_by_3_and_5())
+def test_number_divisible_by_3_and_5_returns_fizzbuzz(num: int):
+    assert fizzbuzz(num) == "fizzbuzz"
+
+
+@given(num=numbers_not_divisible_by_3_or_5())
+def test_number_not_divisible_by_3_or_5_returns_number(num: int):
+    assert fizzbuzz(num) == str(num)
