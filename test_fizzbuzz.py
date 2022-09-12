148a7b2d7e026a01bcd8e2f54c0cc5b1738c215d
Troy Moench <brewersarenumber1@gmail.com>
2022-09-11 21:29:19 -0500

    Use sampled_from strategy instead of integers for numbers divisible by 3 and 5

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,4 +1,4 @@
-from hypothesis import HealthCheck, given, settings
+from hypothesis import given
 from hypothesis import strategies as st
 
 from fizzbuzz import fizzbuzz
@@ -17,9 +17,8 @@ def numbers_divisible_by_5_but_not_3():
 
 
 def numbers_divisible_by_3_and_5():
-    return st.integers(min_value=1, max_value=100).filter(
-        lambda x: x % 5 == 0 and x % 3 == 0
-    )
+    # Avoid HealthCheck.filter_too_much exception when using integers
+    return st.sampled_from([15, 30, 45, 60, 75, 90])
 
 
 def numbers_not_divisible_by_3_or_5():
@@ -39,7 +38,6 @@ def test_number_divisible_by_5_but_not_3_returns_buzz(num: int):
 
 
 @given(num=numbers_divisible_by_3_and_5())
-@settings(suppress_health_check=[HealthCheck.filter_too_much])
 def test_number_divisible_by_3_and_5_returns_fizzbuzz(num: int):
     assert fizzbuzz(num) == "fizzbuzz"
 
